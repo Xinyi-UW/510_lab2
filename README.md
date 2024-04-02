15cc87b6adfd6d77b070b8e5fe8fdc6d9160c000
Xinyi Zhou <xinyigix@uw.edu>
2024-04-02 16:45:15 -0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 This is the README file.
+
+#This is 510 lab2 assingment including data visualization and analysis
+
+```
+Getting Started
+
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+
+Some debug experiences
+
+Connecting local git and remote githut repository everytime encounter some similar problem.
+Then, trying to polish the process:
+1.Create a new repository on your github with README.md
+2.Git clone
+3.Create app.py, new stuff on vs code, then push them to repository to check how does it wrok
+
+
+```
+
+I wish to upload urban data to analyse some building price and street activities, also visualize them in a proper way (hopefully on a map)
--- app.py
+++ app.py
@@ -1,1 +1,43 @@
-#dede
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+st.set_page_config(
+    page_title="play_ground",
+    page_icon="🎈",
+    layout="centered"
+)
+
+
+# Load the dataset
+# Ensure the dataset is in the same directory as this script, or provide the full path.
+df = pd.read_csv('https://raw.githubusercontent.com/mcnakhaee/palmerpenguins/master/palmerpenguins/data/penguins.csv')
+
+st.title("Database_playground")
+st.markdown("""
+This web app allows users to explore and visualize data interactively. 
+Utilize the sidebar to filter the dataset and customize the visualizations.
+""")
+
+data_filter = st.slider('What do you want?')
+st.write("city:")
+
+# Define a list of options for the multiselect dropdown
+options = ['Option 1', 'Option 2', 'Option 3']
+
+# Use st.multiselect to allow the user to select multiple options
+selected_options = st.multiselect('Select your options:', options)
+
+# Display the selected options
+st.write('You selected:', selected_options)
+
+
+ 
+
+
+
+
+
+
